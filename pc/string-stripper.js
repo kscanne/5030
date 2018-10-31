595cbdc0c5466a5d1099a33b041692a9b2cc791e
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2018-10-31 17:41:45 -0500

    Fix code to pass tests 7-9, 18

--- pc/string-stripper.js
+++ pc/string-stripper.js
@@ -3,7 +3,7 @@ var util = require('./util.js');
 var getAvailableLanguages = function () {
     var availableLanguageList = util.readTextFileToArray('../data/list.txt');
     if (availableLanguageList.indexOf(languageCode) === -1) {
-        console.error(`Language '${languageCode}' is not supported.`);
+        // console.error(`Language '${languageCode}' is not supported.`);
         throw new Error("Not a valid language code");
     }
 }
@@ -18,12 +18,8 @@ exports.stripStopWords = function (languageCode, stringToStrip) {
         var stopWordList = getStopWords(languageCode);
         var inputWordList = (stringToStrip || "").trim().split(new RegExp(/\s+/));
 
-        stopWordList.forEach(function (stopWord, stopWordIndex) {
-            inputWordList.forEach(function (word, wordIndex) {
-                if (stopWord.toLowerCase() === word.toLowerCase()) {
-                    inputWordList.splice(wordIndex, 1);
-                }
-            });
+        stopWordList.forEach(function (stopWord) {
+            inputWordList = inputWordList.filter(word => stopWord.toLowerCase() !== word.toLowerCase());
         });
 
         return inputWordList.join(" ");
