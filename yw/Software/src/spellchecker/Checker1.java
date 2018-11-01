8e1c56340102bf15426bb75636df9dda864e4f3a
nyrabbit <yifei.wang@slu.edu>
2018-11-01 10:57:42 -0500

    word_strip 2.0

--- yw/Software/src/spellchecker/Checker1.java
+++ yw/Software/src/spellchecker/Checker1.java
@@ -1,32 +1,107 @@
 package spellchecker;
 
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Checker1 {
+	
+
 
 	
-  public  String check(String a, String b) {
+  public  String check(String a) throws IOException {
+	  
+	  Read i = new Read();
+	  List<String> s = new ArrayList();
+	  
+	  System.out.println("read method start working");
+
+	  s = i.read();  // S is now a line of string that need to be get rid of
+	  String b = a.toLowerCase();  //change everything to lowercase
+	  String [] Arr = a.split("\\s+");  // split the original with space
+	  String [] arr = b.split("\\s+");  // split the line with space
+	  
+
+      List<String> Ar= Arrays.asList(Arr);
+	  System.out.println("core striping start working! ...");
+	  List<Integer> suspect = new ArrayList();
+	  
+	  for(int e = 0;e<arr.length;e++) {
+		  System.out.println(Arr[e]);
+		  for(String sc : s) {
+			  
+			  if(arr[e].equals(sc)) {
+				  
+				  System.out.println(Ar + " " + e);
+				  System.out.println(Ar.get(e));
+                  suspect.add(e);
+				//Ar.remove(Ar.get(e)) ;
+				  System.out.println(Ar);
+
+			  }
+		  }
+		  
+	  }
+	  System.out.println(suspect);
+	  List<String> copyOfArr = new ArrayList();
+	  for(int e=0;e<Arr.length;e++) {
+		  if(suspect.indexOf(e)==-1) {
+			  copyOfArr.add(Arr[e]);
+		  }
+	  }
+	  String x = String.join(" ", copyOfArr);  
+	  System.out.println("map all the remain element...");
+	  
+	  System.out.println(x + "!!!!!");
+	  
+	return x;
 		
-		if("en".equals(a)) {
-			b = b.replaceAll("the |The | the", "");
-			b = b.replaceAll(" to|to |To ", "");
-			b = b.replaceAll(" the ", " ");
-			b = b.replaceAll(" to ", " ");
-			
-		}else if("ga".equals(a)) {
-			b = b.replaceAll("Na | na|na ", "");
-			b = b.replaceAll(" na ", " ");
-			
-		}else if("fr".equals(a)) {
-			b = b.replaceAll(" la|la |La ", "");
-			b = b.replaceAll(" la ", " ");
-			
-		}else {
-			b = "Still working on it..";
-		}
-		return b;
+
 		
 	}
+  
+  
+  @SuppressWarnings("unchecked")
+public List<String> read() throws IOException {
+	  String target_dir = "C:\\Users\\wangyifei\\software\\5030\\data";
+	  File dir = new File(target_dir);
+	  File[] files = dir.listFiles();
+	  List<String> myList = new ArrayList();
+	  
+	  for (File f : files) {
+		  if(f.isFile()) {
+			  BufferedReader inputStream = null;
+			  
+			  try {
+				  inputStream = new BufferedReader(new FileReader(f));
+				  String line;
+				  
+				  while ((line = inputStream.readLine())!=null) {
+					  myList.add(line);
+					  
+				  }
+			  }
+			  finally {
+				  if (inputStream != null) {
+                      inputStream.close();
+                  }
+			  }
+		  }
+	  }
+	System.out.println(myList);
+	   
+	return myList;
+	
+	  
+  }
+  
+  
+  
+  
 		
 		
 		//source sentences
