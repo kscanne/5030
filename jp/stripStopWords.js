31aa939a7e36b899a5e06f7884603c85f07889a1
Jyckle <jycklepasquesi@gmail.com>
2018-10-31 19:24:03 -0500

    removed the code that shouldn't have been in here, and added all changes from code review

--- jp/stripStopWords.js
+++ jp/stripStopWords.js
@@ -8,14 +8,28 @@ module.exports={
 			return "Error: Not a valid language code"
 		}
 
-		var stopWords = fileSystem.readFileSync('../data/' + languageCode + '.txt').toString().split('\n');
+		var stopWords;
+		try {
+			stopWords = fileSystem.readFileSync('../data/' + languageCode + '.txt').toString().split('\n');
+			stopWords.map(x => {
+				x.toLowerCase(),
+				x.normalize('NFC')}
+				);
+		} catch (err){
+			if (err.code === 'ENOENT'){
+				return "Error: List of Root Words not found";
+			}
+			else{
+				throw err;
+			}
+		} 
 		var splitInput = inputText.toString().split(" ");
 		var finalString= "";
 
-		for (word in splitInput){
-			var found = stopWords.indexOf(splitInput[word].toLowerCase());
+		for (index in splitInput){
+			var found = stopWords.indexOf(splitInput[index].toLowerCase().normalize('NFC'));
 			if (found == -1){
-				finalString = finalString + splitInput[word]+" ";
+				finalString = finalString + splitInput[index]+" ";
 			}
 		}
 
--- jp/testStripStopWords.js
+++ jp/testStripStopWords.js
@@ -3,27 +3,29 @@ var stripFunction = require('./stripStopWords');
 function runTests(){
 	var passedTests=0;
 	var failedTests=0;
+	var testNumber=0;
 	var fileSystem = require('fs');
 	var testData = fileSystem.readFileSync('../tests/cases.tsv').toString().trim().split("\n");
 
 	for(test in testData) {
     	testData[test] = testData[test].toString().split("\t");
-    	if(runTest(testData[test]) == "Fail"){
+    	if(runTest(testData[test],testNumber) == "Fail"){
     		failedTests++;
     	}
-    	else if (runTest(testData[test]) == "Pass"){
+    	else if (runTest(testData[test],testNumber) == "Pass"){
     		passedTests++;
     	}
+    	testNumber++;
 	}
 
 	console.log("Tests Passed: " + passedTests);
 	console.log("Tests Failed: " + failedTests);
 }
 
-function runTest(testCase){
+function runTest(testCase, testNumber){
 	var output = stripFunction.strip_stopwords(testCase[0],testCase[1]);
-	if (output !== testCase[2]){
-		console.log("Error: should have returned: " + testCase[2]);
+	if (output != testCase[2]){
+		console.log("Error for test " + testNumber + ": should have returned: " + testCase[2]);
 		console.log("But instead returned: " + output);
 		return "Fail";
 	}
