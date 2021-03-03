85dbed6d428e6e37496dcf0696b33919291f3698
rebelDi <napalm30@icloud.com>
2021-03-03 14:51:34 -0600

    Added Comments
    Added comments for methods and variables.

--- S21/rebelDi/tests/diana/WordToLowerCaseTest.java
+++ S21/rebelDi/tests/diana/WordToLowerCaseTest.java
@@ -9,6 +9,10 @@ import static org.junit.Assert.*;
 
 public class WordToLowerCaseTest {
 
+    /**
+     * This is the test method for WordToLowerCase class,
+     * that tests the result of changeWordToLowerCase()
+     */
     @Test
     public void changeWordToLowerCase() throws IOException {
         List<String[]> wordsToProcess = openTSV();
@@ -20,40 +24,49 @@ public class WordToLowerCaseTest {
             System.out.println("Word for lowercase: " + wordForLowerCase);
 
             String wordInLowerCase = WordToLowerCase.changeWordToLowerCase(wordForLowerCase, language);
-            assertEquals(correctWordInLowerCase, wordInLowerCase);
+            assertEquals("The word was processed correctly", correctWordInLowerCase, wordInLowerCase);
         }
     }
 
+    /**
+     * This is the function for opening the tsv file with examples of words to convert
+     *
+     * @return all the data from file
+     */
     public static List<String[]> openTSV () throws IOException {
-        List<String[]> wordsToProcess = new ArrayList<>();
+        List<String[]> listOfDataFromFile = new ArrayList<>();
 
-        StringTokenizer st;
-        BufferedReader TSVFile = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\tests\\resources\\tests.tsv"));
-        String dataRow = TSVFile.readLine(); // Read first line.
+        StringTokenizer stringTokenizer; // used to get data from row for each column
+        BufferedReader tsvFile = new BufferedReader(new FileReader
+                (System.getProperty("user.dir") + "\\tests\\resources\\tests.tsv"));
+        String dataRow = tsvFile.readLine(); // Read first line
 
         int iteration = 1;
-        String[] array = new String[3];
+        String[] row = new String[3]; // used to get one line of data to the list
         while (dataRow != null){
-            st = new StringTokenizer(dataRow,"\t");
-            List<String> dataArray = new ArrayList<String>() ;
-            while(st.hasMoreElements()){
-                dataArray.add(st.nextElement().toString());
+            stringTokenizer = new StringTokenizer(dataRow,"\t");
+            List<String> dataArray = new ArrayList<>() ;
+            while(stringTokenizer.hasMoreElements()){
+                dataArray.add(stringTokenizer.nextElement().toString());
             }
 
+
             for (String item:dataArray) {
-                array[iteration - 1] = item;
+                row[iteration - 1] = item; // fill in the array with an item from file
+                // each line in file has 3 items, when the last item added
+                // insert the row into the list of data and empty the row array
                 if (iteration % 3 == 0) {
-                    wordsToProcess.add(array);
+                    listOfDataFromFile.add(row);
                     iteration = 1;
-                    array = new String[3];
+                    row = new String[3];
                 }else {
                     iteration++;
                 }
             }
-            dataRow = TSVFile.readLine(); // Read next line of data.
+            dataRow = tsvFile.readLine(); // Read next line of data
         }
-        // Close the file once all data has been read.
-        TSVFile.close();
-        return wordsToProcess;
+        // Close the file once all data has been read
+        tsvFile.close();
+        return listOfDataFromFile;
     }
 }
