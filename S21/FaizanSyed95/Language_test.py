ecf0a038560f17117bf6f41c9f85a8d97a396915
FaizanSyed95 <ahmed.faizaneng@gmail.com>
2021-03-10 16:32:53 -0600

    Added comments and List name changed

--- S21/FaizanSyed95/Language_test.py
+++ S21/FaizanSyed95/Language_test.py
@@ -1,29 +1,32 @@
 import csv 
 
 Vowels=['A','E','I','O','U','Á','É','Í','Ó','Ú']
-Greek=['Π','Ό','Λ','Η','Σ']
-Greek_again=['π','ό','λ','η','ς']
+GreekCapital=['Π','Ό','Λ','Η','Σ']
+GreekSmall=['π','ό','λ','η','ς']
 
 
-def Word_check(lang , word):
-    LG=list(lang)
+def Word_check(language , word):   #Function which has two inputs as string for checking the word and language
+    Letter=list(language)   #converting string into list for detecting index values
     
-    if LG[0]=='z' and LG[1]=='h' or LG[0]=='J' and LG[1]=='a' or LG[0]=='t' and LG[1]=='h':
+    #Condition for detecting Chinese, Thai and Japanese 
+    if Letter[0]=='z' and Letter[1]=='h' or Letter[0]=='J' and Letter[1]=='a' or Letter[0]=='t' and Letter[1]=='h':
         return word
     
-            
-    elif LG[0]  == "e" and LG[1] == "n" :
+    #Condition for All English        
+    elif Letter[0]  == "e" and Letter[1] == "n" :
             return word.lower()
-        
-    elif  LG[0]=='t' and LG[1]=='r' or LG[0]=='a' and LG[1]=='z'  :
+    
+    #Condition for Turksih and Azerbhaijani
+    elif  Letter[0]=='t' and Letter[1]=='r' or Letter[0]=='a' and Letter[1]=='z'  :
             wordArray= list(word)     
             for i in range(len(wordArray)):   
                 if wordArray[i] == "I":
                      wordArray[i] = "ı"
                      
             return ''.join(wordArray).lower()
-        
-    elif  LG[0]=='g' and LG[1]=='a' :
+    
+    #Condition for Irish    
+    elif  Letter[0]=='g' and Letter[1]=='a' :
         wordArray = list(word)
         for j in range(len(Vowels)):
             if wordArray[0] == "t" or wordArray[0] == "n":
@@ -32,27 +35,28 @@ def Word_check(lang , word):
         
         return ''.join(wordArray).lower()
    
-    elif LG[0]=='e' and LG[1]=='l':
+    #Condition for Greek 
+    elif Letter[0]=='e' and Letter[1]=='l':
         wordArray=list(word)        
         for i in range(len(wordArray)):
-            for j in range(len(Greek)):
-                if wordArray[i] == Greek[j]:
-                    wordArray[i] = Greek_again[j]
+            for j in range(len(GreekCapital)):
+                if wordArray[i] == GreekCapital[j]:
+                    wordArray[i] = GreekSmall[j]
                     
         return ''.join(wordArray)
         
 
 
-
+#Openning and reading the test file
 file = open("C:/Users/Phaixan/Desktop/5030/S21/FaizanSyed95/tests.tsv",  encoding = "utf8")
 read_tsv = csv.reader(file,delimiter="\t") #auto test by reading from the file
 for row in read_tsv: # going through each row of tsv file
     W = Word_check(row[1],row[0])
     #print(W , row[2])
     if W == row[2]:
-        print("Right")
+        print("Word Match")
     else:
-        print("not right")
+        print("No Match")
 
 
 
