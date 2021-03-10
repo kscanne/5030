f4638e3dddf0f014bda0966af3660594f09dc93c
evanswj <willie.evans@slu.edu>
2021-03-10 13:06:04 -0600

    Lang Detect shows full country name

--- S21/hvkone/dev/_conv_low_v1.py
+++ S21/hvkone/dev/_conv_low_v1.py
@@ -1,16 +1,21 @@
 # pip install langdetect
 # pip install translate
+# pip install iso-639
+# pip install pycountry
 
 from langdetect import detect
 from translate import Translator
+from iso639 import languages
+import pycountry 
 
 usr = input('Type Word Here: ')
 # usr_caps = usr.upper()
 # print('Input: ', usr_caps)
 
-lang = detect(usr)
-print('Language Detected: ', lang)
+lang_code = detect(usr)
+lang_name = pycountry.languages.get(alpha_2 = lang_code)
+print('Language Detected: ', lang_name.name, '[',lang_code,']')
 
-trans = Translator(from_lang = lang, to_lang = "english")
+trans = Translator(from_lang = lang_code, to_lang = "english")
 conv_low = trans.translate(usr)
 print('Lowercase Conversion: ', conv_low)
