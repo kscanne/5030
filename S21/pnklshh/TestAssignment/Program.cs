574eb4b81c449e81a1da451b1918276495e43c44
Pankil Shah <pnklshh06@hotmail.com>
2021-03-11 12:48:38 -0600

    Added program and test code

---
+++ S21/pnklshh/TestAssignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TestAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var runAllTests = false;    // To run all tests together, change the value to true
+
+            if (runAllTests)
+                Test.PerformTest();
+            else
+            {
+                Console.WriteLine("Enter Word: ");
+                string word = Console.ReadLine();
+                Console.WriteLine("Enter Language: ");
+                string language = Console.ReadLine();
+                var lowerWord = ConvertToLowercase(word, language);
+                Console.WriteLine("Lower word: " + lowerWord);
+            }
+        }
+
+        public static string ConvertToLowercase(string word, string language)
+        {
+            var charList = new List<char>(word);
+            var lowerCharList = new List<char>();
+            var lowerWord = "";
+            var culture = new CultureInfo(language);
+            var vowelList = new List<char>() { 'A', 'E', 'I', 'O', 'U', 'Á', 'É', 'Í', 'Ó', 'Ú' };
+            const string IRISH_CODE = "ga";
+            const string Greek_CODE = "el";
+
+            for (var i = 0; i < charList.Count; i++)
+            {
+                lowerCharList.Add(char.ToLower(charList[i], culture));
+            }
+
+            if (language.StartsWith(IRISH_CODE)                     // handle exception for Irish(if a word begins with a lowercase n or t immediately followed by an uppercase vowel, then a hyphen must be inserted as the second character when lowercasing)
+                && (charList[0] == 'n' || charList[0] == 't')
+                && (vowelList.Contains(charList[1])))
+            {
+                lowerCharList.Insert(1, '-');
+            }
+            else if(language.StartsWith(Greek_CODE) && charList[charList.Count - 1] == 'Σ')   // handle exception for Σ as last character in Greek
+            {
+                lowerCharList[charList.Count - 1] = 'ς';
+            }
+
+            foreach (var character in lowerCharList)
+            {
+                lowerWord += character.ToString();
+            }
+
+            return lowerWord;
+        }
+    }
+}
---
+++ S21/pnklshh/TestAssignment/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TestAssignment
+{
+    class Test
+    {
+        public static void PerformTest()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Enter the path of tsv file: ");
+            var path = Console.ReadLine();
+            var testInput = File.ReadAllLines(path);
+            Console.WriteLine("\n Output for each test case: \n");
+
+            foreach (var input in testInput)
+            {
+                var word = input.Split('\t')[0];
+                var language = input.Split('\t')[1];
+
+                var lowerWord = Program.ConvertToLowercase(word, language);
+                Console.WriteLine(word + '\t' + language + '\t' + lowerWord);
+            }
+        }
+    }
+}
