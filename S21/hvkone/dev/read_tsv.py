7898aac99a36a46e68443d00bbd7f91b15b3bb77
evanswj <willie.evans@slu.edu>
2021-03-12 02:17:03 -0600

    Added Compiled File

--- S21/hvkone/dev/read_tsv.py
+++ S21/hvkone/dev/read_tsv.py
@@ -1,16 +1,25 @@
-fs="\t"
+import pandas as pd
+
+fs = "\t"
 table = str.maketrans('\t', fs)
-fName = 'tests.tsv'
-f = open(fName,'r')
 
-try:
-  line = f.readline()
-  while line:
-    print(line.translate(table), end = "")
-    line = f.readline()
+fName = 'tests.csv'
+file_read = pd.read_csv(fName, sep = '\t')
 
-except IOError:
-  print("Could not read file: " + fName)
+# f = open(fName,'r')
+# with open(fName,'r') as tsv:
+#     word_input = (col[1])
+#     lang_input = (col[2])
+#     exp_output = (col[3]);  
 
-finally:
-  f.close()
+# try:
+#   line = f.readline()
+#   while line:
+#     print(line.translate(table), end = "")
+#     line = f.readline()
+
+# except IOError:
+#   print("Could not read file: " + fName)
+
+# finally:
+#   f.close()
--- S21/hvkone/dev/ui_txt_conv.py
+++ S21/hvkone/dev/ui_txt_conv.py
@@ -1,4 +1,15 @@
-# pip install python-bcp47
+#---------------------------------
+# DEPENDECIES
+#---------------------------------
+# pip install langdetect
+from langdetect import detect
+
+# pip install translate
+from translate import Translator
+
+# pip install pycountr
+import pycountry
+
 # pip install bcp47
 from bcp47 import bcp47
 
@@ -6,15 +17,28 @@ from bcp47 import bcp47
 # pip install language_data
 from langcodes import *
 
-# pip install chardet
 
-
-#------------------------------------
+def convert_to_ascii(text):
+    return " ".join(str(ord(char)) for char in text)
+#---------------------------------
 # START OF CODE
-#------------------------------------
+#---------------------------------
 usr_word = input('Type Word (to be converted to lowercase): ')
+
 usr_word_code = usr_word.encode(encoding = 'UTF-8',errors = 'strict')
 print('UTF-8 Input: ', usr_word_code)
+
+usr_asc = convert_to_ascii(usr_word)
+print('ORIGINAL ASCII INPUT: ', usr_asc)
+# Convert to lowercase using ASCII 
+usr_asc_new = ''
+for i in range(len(usr_word)):
+    if(usr_word[i] >= 'A' and usr_word[i] <= 'Z'):
+        usr_asc_new = usr_asc_new + chr((ord(usr_word[i]) + 32))
+    else:
+        usr_asc_new = usr_asc_new + usr_word[i]
+usr_asc_new = convert_to_ascii(usr_asc_new)       
+print('CONVERTED ASCII OUTPUT: ', usr_asc_new)
 
 usr_lang = input('Type Language Code [en]: ')
 usr_regn = input('Type Region Code [US]: ')
@@ -35,13 +59,14 @@ print('Language Code: ', lang_code)
 lang_name = Language.make(language = lang_code).display_name()
 print('Language Name: ', lang_name)
 
-# lang_tags = bcp47(lang_code)
+#---------------------------------
+# LANGUAGE CONDITIONALS
+#---------------------------------
 
 # IF GAELIC INPUT
-if lang_code == 'ga' and (usr_word.startswith('t') or usr_word.startswith('n')) and usr_word[1] in ['A','E','I','O','U']:
+if lang_code == 'ga' and (usr_word.startswith('t') or usr_word.startswith('n')) and usr_word[1] in ['A','E','I','O','U','Á','É','Í','Ó','Ú']:
     conv_word = '-'.join([usr_word[0],usr_word[1:]])
     print('Hyphenated Lowercase Conversion: ', conv_word.lower())
 else:
     conv_word = usr_word_code.lower()
     print('Lowercase Conversion: ', conv_word)
-
