ffadea434055ed2ef8d266b76aba7144658b515f
Pankil Shah <pnklshh06@hotmail.com>
2021-04-07 11:11:55 -0500

    Refactored code

--- S21/pnklshh/refactorme.py
+++ S21/pnklshh/refactorme.py
@@ -1,84 +1,108 @@
 import unicodedata
 
-class Word:
+class Word(object):
 
-  def __init__(self, word, bcpCode, std=False):
-    self._w = word
-    self._l = bcpCode
+  def __init__(self, word, bcpCode):
+    self._word = word
+    self._language = bcpCode
     self._finalSigma = False
-    self._standardIrishSpelling = std
-    # OLD EXPERIMENTAL CODE for dealing with vowel harmony
-    # self._numVowels = 0
-    # for c in word:
-    #   if c in 'aeiouAEIOU':
-    #   self._numVowels += 1
 
-  def setWord(self, w):
-    self._w = w
+  def performIrishCheck(self):
+    return ((self._word[0]=='t' or self._word[0]=='n') and unicodedata.normalize('NFC', self._word)[1] in 'AEIOUÁÉÍÓÚ')
 
   def toLower(self):
-    language = self._l
-    if '-' in self._l:
-      i = self._l.find('-')
-      language = self._l[0:i]
-    if len(language)<2 or len(language)>3:
-      print("Invalid BCP-47 code")
-      return ''
-    temp = self._w
-    if language=='zh':
-      return temp
-    elif language=='ja':
-      return temp
-    elif language=='ga':
-      if len(self._w)>1:
-        if (self._w[0]=='t' or self._w[0]=='n') and unicodedata.normalize('NFC', self._w)[1] in 'AEIOU\u00c1\u00c9\u00cd\u00d3\u00da':
-          temp = self._w[0]+'-'+temp[1:]
-      return temp.lower()
-    elif language=='tr':
-      temp = self._w
-      temp = temp.replace('\u0049','\u0131')
-      return temp.lower()
-    elif language=='az':
-      temp = self._w
-      temp = temp.replace('\u0049','\u0131')
-      return temp.lower()
-    elif language=='th':
-      return temp
-    elif language=='el':
-      if temp[-1]=='\u03a3':
-        self._finalSigma = True
-        temp = temp[:-1]+'\u03c2'
-      return temp.lower()
-    elif False and language=='gd':
-      # specification doesn't ask for this language to be treated differently
-      # so this will never be called
-      if len(self._w)>1:
-        if (self._w[0]=='t' or self._w[0]=='n') and self._w[1] in 'AEIOU\u00c1\u00c9\u00cd\u00d3\u00da':
-          temp = self._w[0]+'-'+temp[1:]
-      return temp.lower()
-    else:
-      return temp.lower()
+      return self._word.lower()
+
+class IrishWord(Word):
+
+  def __init__(self, word, bcpCode, std=False):
+    Word.__init__(self, word, bcpCode)
+    self._standardIrishSpelling = std
+
+  def toLower(self):
+    if len(self._word)>1:
+        if (self.performIrishCheck()):
+          self._word = self._word[0]+'-'+self._word[1:]
+    return self._word.lower()
 
   def isLenited(self):
-    language = self._l
-    if '-' in self._l:
-      i = self._l.find('-')
-      language = self._l[0:i]
+    language = self._language
+    if '-' in self._language:
+      i = self._language.find('-')
+      language = self._language[0:i]
     if language == 'ga' or language == 'gd':
-      if len(self._w) < 2:
+      if len(self._word) < 2:
         return False
       else:
-        return self._w[0].lower() in 'bcdfgmpst' and self._w[1].lower()=='h'
+        return self._word[0].lower() in 'bcdfgmpst' and self._word[1].lower()=='h'
     else:
       raise NotImplementedError('Method only available for Irish and Scottish Gaelic')
+
+class GreekWord(Word):
+
+  def __init__(self, word, bcpCode):
+    Word.__init__(self, word, bcpCode)
+
+  def toLower(self):
+    if self._word[-1]=='Σ':
+        self._finalSigma = True
+        self._word = self._word[:-1]+'ς'
+    return self._word.lower()
+
+class TurkishWord(Word):
+
+  def __init__(self, word, bcpCode):
+    Word.__init__(self, word, bcpCode)
+
+  def toLower(self):
+    self._word = self._word.replace('I','ı')
+    return self._word.lower()
+
+class AzerbaijaniWord(Word):
+
+  def __init__(self, word, bcpCode):
+    Word.__init__(self, word, bcpCode)
+
+  def toLower(self):
+    self._word = self._word.replace('I','ı')
+    return self._word.lower()
+
+class InvalidLanguageException(Exception):
+    def __init__(self, message):            
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+def isInvalidLanguage(language):
+    return (len(language)<2 or len(language)>3)
+
+def languageCanBeIgnored(language):
+    return (language=='zh' or language=='ja' or language=='th')
 
 if __name__=='__main__':
   f = open('tests.tsv')
   for line in f:
     line = line.rstrip('\n')
-    pieces = line.split('\t')
-    w = Word(pieces[0], pieces[1])
-    answer = w.toLower()
-    if answer != pieces[2]:
-      print('Test case failed. Expected', pieces[2], 'when lowercasing',pieces[0],'in language',pieces[1],'but got',answer)
+    (word, language, result) = line.split('\t')
+    
+    if '-' in language:
+      index = language.find('-')
+      language = language[0:index]
+    if (isInvalidLanguage(language)):
+      raise InvalidLanguageException("Invalid BCP-47 code", )
+
+    if (languageCanBeIgnored(language)):
+      answer = word
+    elif language=='ga':
+      answer = IrishWord(word, language).toLower()   
+    elif language=='tr':  
+      answer = TurkishWord(word, language).toLower()   
+    elif language=='az':
+      answer = AzerbaijaniWord(word, language).toLower()
+    elif language=='el':
+      answer = GreekWord(word, language).toLower()  
+    else:
+      answer = Word(word, language).toLower()
+
+    if answer != result:
+      print('Test case failed. Expected', result, 'when lowercasing',word,'in language',language,'but got',answer)
   f.close()
