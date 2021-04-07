faf60ac0cdfe358f5e4f55b3786c9f4a41d131d4
Zhen Guo <zhen.guo.2@slu.edu>
2021-04-08 00:32:04 +0800

    refator after group discussion

--- S21/jackzhenguo/refactorme.py
+++ S21/jackzhenguo/refactorme.py
@@ -1,84 +1,112 @@
 import unicodedata
 
+
 class Word:
+    def __init__(self, word, bcp_code):
+        self.wc = word
+        self.lang = bcp_code
 
-  def __init__(self, word, bcpCode, std=False):
-    self._w = word
-    self._l = bcpCode
-    self._finalSigma = False
-    self._standardIrishSpelling = std
-    # OLD EXPERIMENTAL CODE for dealing with vowel harmony
-    # self._numVowels = 0
-    # for c in word:
-    #   if c in 'aeiouAEIOU':
-    #   self._numVowels += 1
 
-  def setWord(self, w):
-    self._w = w
+class Strategy(object):
+    def __init__(self, word: Word):
+        self.word = word
+    """
+    strategy interface
+    """
+    def process(self):
+        pass
 
-  def toLower(self):
-    language = self._l
-    if '-' in self._l:
-      i = self._l.find('-')
-      language = self._l[0:i]
-    if len(language)<2 or len(language)>3:
-      print("Invalid BCP-47 code")
-      return ''
-    temp = self._w
-    if language=='zh':
-      return temp
-    elif language=='ja':
-      return temp
-    elif language=='ga':
-      if len(self._w)>1:
-        if (self._w[0]=='t' or self._w[0]=='n') and unicodedata.normalize('NFC', self._w)[1] in 'AEIOU\u00c1\u00c9\u00cd\u00d3\u00da':
-          temp = self._w[0]+'-'+temp[1:]
-      return temp.lower()
-    elif language=='tr':
-      temp = self._w
-      temp = temp.replace('\u0049','\u0131')
-      return temp.lower()
-    elif language=='az':
-      temp = self._w
-      temp = temp.replace('\u0049','\u0131')
-      return temp.lower()
-    elif language=='th':
-      return temp
-    elif language=='el':
-      if temp[-1]=='\u03a3':
-        self._finalSigma = True
-        temp = temp[:-1]+'\u03c2'
-      return temp.lower()
-    elif False and language=='gd':
-      # specification doesn't ask for this language to be treated differently
-      # so this will never be called
-      if len(self._w)>1:
-        if (self._w[0]=='t' or self._w[0]=='n') and self._w[1] in 'AEIOU\u00c1\u00c9\u00cd\u00d3\u00da':
-          temp = self._w[0]+'-'+temp[1:]
-      return temp.lower()
-    else:
-      return temp.lower()
 
-  def isLenited(self):
-    language = self._l
-    if '-' in self._l:
-      i = self._l.find('-')
-      language = self._l[0:i]
-    if language == 'ga' or language == 'gd':
-      if len(self._w) < 2:
-        return False
-      else:
-        return self._w[0].lower() in 'bcdfgmpst' and self._w[1].lower()=='h'
-    else:
-      raise NotImplementedError('Method only available for Irish and Scottish Gaelic')
+class StrategyBase(Strategy):
+    def process(self) -> str:
+        """
+        strategy of only lower process
+        :return:
+        """
+        return self.word.wc.lower()
 
-if __name__=='__main__':
-  f = open('tests.tsv')
-  for line in f:
-    line = line.rstrip('\n')
-    pieces = line.split('\t')
-    w = Word(pieces[0], pieces[1])
-    answer = w.toLower()
-    if answer != pieces[2]:
-      print('Test case failed. Expected', pieces[2], 'when lowercasing',pieces[0],'in language',pieces[1],'but got',answer)
-  f.close()
+
+class StrategyKeep(Strategy):
+    def process(self) -> str:
+        """
+        strategy of input word same to output word
+        :return:
+        """
+        return self.word.wc
+
+
+class StrategyVowel(Strategy):
+    """
+    strategy of processing to vowel cases
+    """
+    def process(self) -> str:
+        vowels = 'AEIOU\u00c1\u00c9\u00cd\u00d3\u00da'
+        temp = self.word.wc
+        if len(temp) > 1 and (self.word.wc[0] in ['t', 'n']) and \
+                unicodedata.normalize('NFC', self.word.wc)[1] in vowels:
+            temp = self.word.wc[0] + '-' + temp[1:]
+        return temp.lower()
+
+
+class StrategyCharReplace(Strategy):
+    def process(self) -> str:
+        """
+        strategy of replacing single char
+        :return:
+        """
+        temp = self.word.wc
+        temp = temp.replace('\u0049', '\u0131')
+        return temp.lower()
+
+
+class StrategyTrail(Strategy):
+    """
+    strategy of processing trail char
+    """
+    def process(self):
+        temp = self.word.wc
+        if temp[-1] == '\u03a3':
+            temp = temp[:-1] + '\u03c2'
+        return temp.lower()
+
+
+class WordProcess(object):
+    def __init__(self, word: Word):
+        self.word = word
+        self.strategy_dict = {'en': StrategyBase, 'zh': StrategyKeep, 'ja': StrategyKeep,
+                              'th': StrategyKeep, 'ga': StrategyVowel, 'tr': StrategyCharReplace,
+                              'az': StrategyCharReplace, 'el': StrategyTrail}
+
+    def to_lower(self):
+        language = self.convert_lang()
+        if language in self.strategy_dict:
+            specific_strategy = self.strategy_dict[language]
+        else:
+            specific_strategy = StrategyBase  # default strategy
+        return specific_strategy(self.word).process()
+
+    def convert_lang(self) -> str:
+        """
+        convert one language with '-'. If not, directly return itself
+        :return: language code after converting
+        """
+        language = self.word.lang
+        if '-' in self.word.lang:
+            i = self.word.lang.find('-')
+            language = self.word.lang[0:i]
+        if len(language) < 2 or len(language) > 3:
+            raise ValueError("Invalid BCP-47 code")
+        return language
+
+
+if __name__ == '__main__':
+    f = open('tests.tsv')
+    for line in f:
+        line = line.rstrip('\n')
+        pieces = line.split('\t')
+        w = Word(pieces[0], pieces[1])
+        answer = WordProcess(w).to_lower()
+        if answer != pieces[2]:
+            print(f"""Test case failed. Expected {pieces[2]}, when lowercasing {pieces[0]} in language {pieces[1]}, 
+            but got {answer}""")
+    f.close()
