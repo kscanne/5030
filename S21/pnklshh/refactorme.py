d98ddecf5d1cd80596c72189ad1cdf6168ff2a64
Pankil Shah <pnklshh06@hotmail.com>
2021-04-07 15:03:40 -0500

    Some more refactoring done

--- S21/pnklshh/refactorme.py
+++ S21/pnklshh/refactorme.py
@@ -5,10 +5,6 @@ class Word(object):
   def __init__(self, word, bcpCode):
     self._word = word
     self._language = bcpCode
-    self._finalSigma = False
-
-  def performIrishCheck(self):
-    return ((self._word[0]=='t' or self._word[0]=='n') and unicodedata.normalize('NFC', self._word)[1] in 'AEIOUÁÉÍÓÚ')
 
   def toLower(self):
       return self._word.lower()
@@ -19,29 +15,33 @@ class IrishWord(Word):
     Word.__init__(self, word, bcpCode)
     self._standardIrishSpelling = std
 
+  def specialCase(self):
+    return ((self._word[0] in 'tn') and unicodedata.normalize('NFC', self._word)[1] in 'AEIOUÁÉÍÓÚ')
+
   def toLower(self):
     if len(self._word)>1:
-        if (self.performIrishCheck()):
+        if (self.specialCase()):
           self._word = self._word[0]+'-'+self._word[1:]
     return self._word.lower()
 
   def isLenited(self):
     language = self._language
     if '-' in self._language:
-      i = self._language.find('-')
-      language = self._language[0:i]
+      index = self._language.find('-')
+      language = self._language[0:index]
     if language == 'ga' or language == 'gd':
       if len(self._word) < 2:
         return False
       else:
         return self._word[0].lower() in 'bcdfgmpst' and self._word[1].lower()=='h'
     else:
-      raise NotImplementedError('Method only available for Irish and Scottish Gaelic')
+      raise InvalidMethodException('Method only available for Irish and Scottish Gaelic')
 
 class GreekWord(Word):
 
   def __init__(self, word, bcpCode):
     Word.__init__(self, word, bcpCode)
+    self._finalSigma = False
 
   def toLower(self):
     if self._word[-1]=='Σ':
@@ -68,41 +68,61 @@ class AzerbaijaniWord(Word):
     return self._word.lower()
 
 class InvalidLanguageException(Exception):
-    def __init__(self, message):            
-        # Call the base class constructor with the parameters it needs
+    def __init__(self, message):
         super().__init__(message)
 
-def isInvalidLanguage(language):
-    return (len(language)<2 or len(language)>3)
+class InvalidMethodException(Exception):
+    def __init__(self, message):            
+        super().__init__(message)
 
-def languageCanBeIgnored(language):
-    return (language=='zh' or language=='ja' or language=='th')
+class Language:
+
+  def __init__(self, bcpCode):
+    self._language = bcpCode
+    self._skipLanguageList = ['zh', 'ja', 'th']
+
+  def getLanguage(self):
+    return self._language
+
+  def getBaseLanguage(self):
+    language = self._language
+    if '-' in language:
+      index = language.find('-')
+      language = language[0:index]
+    return language
+
+  def isInvalidLanguage(self):
+    baselanguage = self.getBaseLanguage()    
+    return (len(baselanguage)<2 or len(baselanguage)>3)
+
+  def languageCanBeIgnored(self):
+      language = self.getBaseLanguage()
+      return (language in self._skipLanguageList)
 
 if __name__=='__main__':
   f = open('tests.tsv')
   for line in f:
     line = line.rstrip('\n')
-    (word, language, result) = line.split('\t')
+    (word, bcpCode, result) = line.split('\t')
     
-    if '-' in language:
-      index = language.find('-')
-      language = language[0:index]
-    if (isInvalidLanguage(language)):
-      raise InvalidLanguageException("Invalid BCP-47 code", )
+    language = Language(bcpCode)
+    if (language.isInvalidLanguage()):
+      raise InvalidLanguageException("Invalid BCP-47 code")
 
-    if (languageCanBeIgnored(language)):
-      answer = word
-    elif language=='ga':
-      answer = IrishWord(word, language).toLower()   
-    elif language=='tr':  
-      answer = TurkishWord(word, language).toLower()   
-    elif language=='az':
-      answer = AzerbaijaniWord(word, language).toLower()
-    elif language=='el':
-      answer = GreekWord(word, language).toLower()  
+    baseLanguage = language.getBaseLanguage()
+    if (language.languageCanBeIgnored()):
+      answer = word.lower()
+    elif baseLanguage=='ga':
+      answer = IrishWord(word, baseLanguage).toLower()   
+    elif baseLanguage=='tr':  
+      answer = TurkishWord(word, baseLanguage).toLower()   
+    elif baseLanguage=='az':
+      answer = AzerbaijaniWord(word, baseLanguage).toLower()
+    elif baseLanguage=='el':
+      answer = GreekWord(word, baseLanguage).toLower()  
     else:
-      answer = Word(word, language).toLower()
+      answer = Word(word, baseLanguage).toLower()
 
     if answer != result:
-      print('Test case failed. Expected', result, 'when lowercasing',word,'in language',language,'but got',answer)
+      print('Test case failed. Expected', result, 'when lowercasing',word,'in language',baseLanguage,'but got',answer)
   f.close()
