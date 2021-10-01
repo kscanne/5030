d828361e1fae750712c64d944479e9ab4ada1cbf
Greih Murray <murrayg0216@outlook.com>
2021-10-01 16:09:56 -0500

    Adding additional tests
    Added additional tests based on those presented in tests.tsv

--- F21/greihmurray/LowerCaser/main_tests.py
+++ F21/greihmurray/LowerCaser/main_tests.py
@@ -5,3 +5,7 @@ class TestLowerCase(unittest.TestCase):
     def testEnglish(self):
         self.assert_(main.lowercase('en', 'This'), 'this')
         self.assert_(main.lowercase('en', 'hElLO tHEre'), 'hello there')
+        self.assert_(main.lowercase('en', 'HELLO'), 'hello')
+        self.assert_(main.lowercase('en-US', 'WORLD'), 'world')
+        self.assert_(main.lowercase('en-IE', 'cAmEl'), 'camel')
+        self.assert_(main.lowercase('en-Latn', '--OK'), '--ok')
