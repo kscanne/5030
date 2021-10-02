d3a14f8528021b480cb4139c96b35977b097f6e5
Andrew Beiler <andrewbeiler@Andrews-Air.attlocal.net>
2021-10-02 07:24:23 -0500

    Added Language Specific Cases Checking language against a list of case insensitive languages. Checking the language to see if it is one of the special cases that needs to be handled.     Using regular expressions to both identify and replace character(s) as needed. Passing all test cases so far with the exception of line 14 (original 13).

--- F21/abeiler7/to_lower_test.py
+++ F21/abeiler7/to_lower_test.py
@@ -9,7 +9,7 @@ class TestToLower(unittest.TestCase):
     def test_something(self):
         for idx, row in test_cases.iterrows():
             #print(row[0])
-            self.assertEqual(lc.to_lower(row[0]), row[2]) # add assertion here
+            self.assertEqual(lc.to_lower(row[0], row[1]), row[2]) # add assertion here
 
 
 if __name__ == '__main__':
--- F21/abeiler7/to_lowercase.py
+++ F21/abeiler7/to_lowercase.py
@@ -1,3 +1,23 @@
-def to_lower(word):
+import re
 
-    return word.lower()
+
+def to_lower(word, lang):
+    case_insensitive_langs = r'(zh)|(ja)|(th)'
+    if not re.compile(case_insensitive_langs).match(lang):
+        results = language_select(word, lang)
+    else:
+        results = word
+
+    return results
+
+def language_select(word, lang):
+    if re.compile("tr*").match(lang) or re.compile("az*").match(lang):
+        updt_word = word.replace('I','\u0131')
+    elif re.compile("ga*").match(lang):
+        updt_word = re.sub(r'\b([nt])([AEI\u004FU\u00C1\u00C9\u00CD\u00D3\u00DA])', r'\1-\2', word)
+    elif re.compile("el*").match(lang):
+        updt_word = re.sub('.*\u0370\b', '\u03A3', word)
+    else:
+        updt_word = word
+
+    return updt_word.lower()
