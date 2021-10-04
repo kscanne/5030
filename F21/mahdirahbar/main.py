c945885c2b4f80ca1fabafe15fb0ae43f9715864
Mahdi <mahdirahbar01@gmail.com>
2021-10-03 19:11:48 -0500

    added the main file as the root of the program

---
+++ F21/mahdirahbar/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# lower
+# Developers: Mahdi Rahbar
+# License: - 
+
+
+import lower
+
+if __name__ == "__main__":
+    lower = lower.Lower("sample", 'en')
+    print(lower.string," - ",lower.lang)
+
