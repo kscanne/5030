004fc804c36e2f12a29f104f98757f2d7f7be278
Marshall Okafor <okwudilichukwu.okafor@slu.edu>
2021-10-06 00:28:34 -0500

    Added a python file and read me file that explains how to run the code

---
+++ F21/marshallokafor/README_2.md
@@ -0,0 +1,7 @@
+How Use The word_to_lowercase.y Program
+-----------------------------
+
+1. Make sure you have Python installed on your PC
+2. Make sure you have lowercase.py and tests.tsv in the same folder or directory
+3. On VScode or any other IDE, run the word_to_lowercase.py program
+4. If you are using a terminal, make sure to use "python" or "python3" command to run the word_to_lowercase.py program 
---
+++ F21/marshallokafor/word_to_lowercase.py
@@ -0,0 +1,94 @@
+
+# List of  languages with applicable lower case rules
+languagesRules = {
+    "tr":  ["lowercase", "dotless_I"],
+    "az":  ["lowercase", "dotless_I"],
+    "ga":  ["second_letter_hyphen", "lowercase"],
+    "el":  ["replace_sigma", "lowercase"],
+    "zh":  ["no_upper_and_lower_cases"],
+    "ja":  ["no_upper_and_lower_cases"],
+    "th":  ["no_upper_and_lower_cases"]
+}
+
+
+def applyWordRule(word, rule):
+
+    if rule == "lowercase":
+        # replace letter 'i' with 'ı'
+        return word.lower()
+
+    if rule == "dotless_I":
+
+        # replace letter 'i' with 'ı'
+        return word.replace('i', 'ı')
+
+    if rule == "second_letter_hyphen":
+
+        # get first letter of word
+        firstCharacter = word[0]
+
+        # check if the letter of word is lowercase n or t
+        if firstCharacter == "n" or firstCharacter == "t":
+            # get second character of the word
+            secondCharacter = word[1]
+
+            # check  if second character is an uppercase irish vowel
+            if secondCharacter.isupper and secondCharacter in ("A", "E", "I", "O", "U", "Á", "É", "Í", "Ó", "Ú"):
+                # convert word to lowercase and insert - as the second character
+                return word[:1]+"-"+word[1:]
+        return word
+
+    if rule == "replace_sigma":
+        # Change all Σ characters to σ unless if Σ is the last character
+        wordWithoutLastCharacter = word[:-1].replace('Σ', 'σ')
+        lastCharacter = word[-1:]
+
+        # check if the last character of word is Σ
+        if lastCharacter == "Σ":
+            lastCharacter = "ς"
+        return wordWithoutLastCharacter+lastCharacter
+
+    if rule == "no_upper_and_lower_cases":
+        # return the original string unchanged
+        return word
+
+
+def wordToLowercase(language, word):
+    language = language.split("-")[0]
+    # Check if language exists in available language rules
+    if language in languagesRules:
+        # Get language and apply language rules
+        languageRules = languagesRules[language]
+        for rule in languageRules:
+            word = applyWordRule(word, rule)
+
+        return word
+    else:
+        return word.lower()
+
+
+def testFile(filePath):
+
+    file = open(filePath, encoding = "utf-8")
+
+    for fileLine in file:
+        # Get line of file and covert it to a list
+        row = fileLine.split("\n")[0].split("\t")
+
+        word = row[0].strip()
+        language = row[1].strip()
+        expectedLowercasedWord = row[2].strip()
+        lowercasedWord = wordToLowercase(language, word).strip()
+
+        if lowercasedWord != expectedLowercasedWord:
+            print( "\033[91m" + "Test Failed  for " + language + " word " + word +
+                " Expected " + expectedLowercasedWord + " but got " + lowercasedWord  + "\033[0m")
+            break   
+        else: 
+            print("\033[92m" +"Word: " + word, "\t \tLanguage: " + language,  "\t Expectation: " +
+            expectedLowercasedWord,  "\t  Result: " + lowercasedWord  + "\033[0m")
+    file.close()
+
+
+# Test file
+testFile("tests.tsv")
