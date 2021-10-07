194b6d1d5b9a11ae375bc33488cfc67f55dc2903
Mahdi <mahdirahbar01@gmail.com>
2021-10-06 23:02:29 -0500

    main file was added as the main file of the program

--- F21/mahdirahbar/main.py
+++ F21/mahdirahbar/main.py
@@ -8,6 +8,8 @@
 import lower
 
 if __name__ == "__main__":
-    lower = lower.Lower("sample", 'en')
-    print(lower.string," - ",lower.lang)
+    text = input("Please enter the sentence you want to lower case: \n\n")
+    lang = input("Please enter your desired language short code (you can look up your desired language short code online - hint: English's short code is \'en\')\n")
+    lower = lower.Lower(text, lang)
+    print("Your chosen language is: ",lower.lang, "\nYour converted text is: ", lower.call_lower())
 
