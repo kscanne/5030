7f5c7babad3463b382f4786a356183a54cdddae8
Andrew Beiler <andrewbeiler@Andrews-Air.attlocal.net>
2021-10-10 04:19:52 -0500

    Addressed Peer Review Comments
    Mostly updates to improve the readability of the code

--- F21/abeiler7/to_lower_test.py
+++ F21/abeiler7/to_lower_test.py
@@ -3,10 +3,8 @@ import pandas as pd
 import to_lowercase as lc
 
 class TestToLower(unittest.TestCase):
-    global test_cases;
-    test_cases = pd.read_csv("./F21/abeiler7/tests.tsv", sep='\t', header=None)
-
     def test_something(self):
+        test_cases = pd.read_csv("./tests.tsv", sep='\t', header=None)
         for idx, row in test_cases.iterrows():
             self.assertEqual(lc.to_lower(row[0], row[1]), row[2])
 
--- F21/abeiler7/to_lowercase.py
+++ F21/abeiler7/to_lowercase.py
@@ -12,12 +12,19 @@ def to_lower(word, lang):
     return results
 
 def language_select(word, lang):
+    # \u00C1 = A with Acute
+    # \u00C9 = E with Acute
+    # \u00CD = I with Acute
+    # \u00D3 = O with Acute
+    # \u00DA = U with Acute
+    irish_regexp = r'\b([nt])([\u00C1\u00C9\u00CD\u00D3\u00DA])'
+
     updt_word = word
     if re.compile("tr*").match(lang) or re.compile("az*").match(lang):
-        updt_word = word.replace('I','\u0131')
+        updt_word = word.replace('I','\u0131') #\u0131 = i with not dot
     elif re.compile("ga*").match(lang):
-        if re.search(r'\b([nt])([\u00C1\u00C9\u00CD\u00D3\u00DA])', word):
-            updt_word = re.sub(r'\b([nt])([\u00C1\u00C9\u00CD\u00D3\u00DA])', r'\1-\2', word)
+        if re.search(irish_regexp, word):
+            updt_word = re.sub(irish_regexp, r'\1-\2', word)
         elif re.search(r'\b([nt])([AEIOU])', word):
             if len(word) > 2 and unicodedata.combining(word[2]) != 0:
                 if re.match(r'[\u00C1\u00C9\u00CD\u00D3\u00DA]', unicodedata.normalize('NFC', word[1]+word[2])):
@@ -25,6 +32,6 @@ def language_select(word, lang):
             else:
                 updt_word = re.sub(r'\b([nt])([AEIOU])', r'\1-\2', word)
     elif re.compile("el*").match(lang):
-        updt_word = re.sub('.*\u0370\b', '\u03A3', word)
+        updt_word = re.sub('.*\u0370\b', '\u03A3', word) # \u0370 = Σ, \u03A3 = ς
 
     return updt_word.lower()
