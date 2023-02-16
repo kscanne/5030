08a165975466a39cb6dfd80d9004cae468c65fba
austin c <ac6404@gmail.com>
2023-02-16 11:53:26 -0600

    add script for lowercasing method

--- S23/austin-carnahan/to_lower.py
+++ S23/austin-carnahan/to_lower.py
@@ -7,11 +7,52 @@
 
 
 def lowercase_me(word, language):
-    print("This code runs!")
+    
+    if language == "ga":
 
+        if word.isupper():
+            return word.lower()
+        
+        if ("A" in word and not word.startswith("A")):
+            return word.replace("A", "-a", 1).lower()
 
+    elif language == "ga-IE":
 
+        # this character won't lowercase. returns none
+        if "Õ" in word:
+            return word.replace("Õ", "!@#").lower().replace("!@#", "õ")
+        
+        if ("Ó" in word and not word.startswith("Ó")):
+            return word.replace("Ó", "-ó").lower()
+
+    elif language == "tr":
+        
+        if "I" in word:
+            return word.lower.replace("i", "ı")
+
+    elif language == "el":
+        return word.lower()
+
+    elif language == "zh-Hans":
+        return word.lower()
+
+    elif language == "th":
+        return word.lower()
+    else:
+        return word.lower()
 
 
 if __name__ == "__main__":
-    lowercase_me("str1", "str2")
+    file = open("tests.tsv")
+
+    for count, value in enumerate(file):
+        codex = value.rstrip('\n').split('\t')
+        
+        result = lowercase_me(codex[0], codex[1])
+
+        if result != codex[2]:
+            print('TEST FAILURE at line' + str(count) + '. Expected ' + codex[2] + '. Found ' + result + ' for language ' + codex[1])
+        else:
+            print('Test Passed for line ' + str(count))
+
+    file.close()
