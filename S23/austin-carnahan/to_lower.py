12d25567db90c0e76cc544bbe9da5d38ae027c42
austin c <ac6404@gmail.com>
2023-02-16 10:47:18 -0600

    add basic code

---
+++ S23/austin-carnahan/to_lower.py
@@ -0,0 +1,17 @@
+
+
+# Create a function that takes an input string and a language as arguments
+# Use conditionals to create special logic based on language
+# Use string manipulation to implement special lowercasing rules
+# Default to standard python lowercasing function
+
+
+def lowercase_me(word, language):
+    print("This code runs!")
+
+
+
+
+
+if __name__ == "__main__":
+    lowercase_me("str1", "str2")
