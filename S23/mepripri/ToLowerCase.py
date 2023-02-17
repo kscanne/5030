53ed85244b7a21091572491bbf459b4e4f5dfe4f
mepripri <mepripri0712@gmail.com>
2023-02-17 15:20:46 -0600

    Changes Made

--- S23/mepripri/ToLowerCase.py
+++ S23/mepripri/ToLowerCase.py
@@ -1,10 +1,8 @@
 import csv
 
-def splitLang(l):
-    l=l.split('-')
-    if len(l[0])==2:
-        l=l[0]
-    return l
+def splitLang(lang):
+    lang=lang.split('-')
+    return lang[0]
 
 def toLower(s,lang):
     str=''
@@ -12,35 +10,27 @@ def toLower(s,lang):
     lang=splitLang(lang)
     if lang=='tr' or lang=='az':
         for i in s:
-            if ord(i)==73:
+            if i=='I':
                 str+=chr(305)
             else:
                 str+=i.lower()
     elif lang=='ga':
-        first=0
-        for i in range(len(s)):
-            if i==0:
-                if (s[0]=='n' or s[0]=='t') and s[1] in ['A','E','I','O','U','Á','É','Í','Ó','Ú']:
-                    str+=s[0].lower()+'-'+s[1].lower()
-                    first=1
-                else:
-                    str+=s[i].lower()
-            elif i==1 and first==1:
-                continue
-            else:
-                str+=s[i].lower()
+        if (s[0]=='n' or s[0]=='t') and s[1] in ['A','E','I','O','U','Á','É','Í','Ó','Ú']:
+            str+=s[0].lower()+'-'+s[1:].lower()
+        else:
+            str+=s.lower()
     elif lang=='el':
         if ord(s[-1])==931:
             str+=s[:-1].lower()
             str+=chr(962)
         else:
             str=str+s.lower()
-    elif lang=='zh' or lang=='th' or lang=='ja' or lang=='en':
-        str+=s.lower()
-    else:
+    elif len(lang)!=2:
         str+='Invalid Language'
+    else:
+        str+=s.lower()
     return str
-
+    
 
 with open("tests.tsv") as file:
     read = csv.reader(file,delimiter="\t")
