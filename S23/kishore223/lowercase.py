f9c201819d8bb51116e4552883463a78a6d45129
kishore223 <kishoremuruganmk@gmail.com>
2023-02-22 16:44:18 -0600

    Unit Test

--- S23/kishore223/lowercase.py
+++ S23/kishore223/lowercase.py
@@ -1,3 +1,5 @@
+import unittest
+
 def lowercase_word(word, lang):
     if lang in ['tr', 'az']:
         return word.replace('I', 'ı').lower()
@@ -18,16 +20,61 @@ def lowercase_word(word, lang):
         return word.lower()
 
 word = input("Enter a word: ")
-
 print("1.Type 'tr' for Turkish Language")
 print("2.Type 'az' for Azerbaijani Language")
 print("3.Type 'ga' for Irish Language")
 print("4.Type 'zh' for Chinese Language(no changes)")
 print("5.Type 'ja' for Japanese Language(no changes)")
 print("6.Type 'th' for Thai Language(no changes)")
-print("****** Note: If you type any other word or character, it will be just converted to lowercase. ******")
+print("****** Note: If you type any other word, it will be just converted to lowercase. ******")
 
 lang = input("Enter language code (BCP-47): ")
 
 lowercased_word = lowercase_word(word, lang)
 print(lowercased_word)
+
+class TestLowercaseWord(unittest.TestCase):
+    def test_turkish_lowercases_word(self):
+        word = "IPHONE"
+        lang = "tr"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "ıphone")
+        
+    def test_azerbaijani_lowercases_word(self):
+        word = "MINI"
+        lang = "az"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "mını")
+        
+    def test_irish_adds_hyphen_to_vowels(self):
+        word = "tAcht"
+        lang = "ga"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "t-acht")
+        
+    def test_chinese_does_not_change_word(self):
+        word = "你好"
+        lang = "zh"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "你好")
+        
+    def test_japanese_does_not_change_word(self):
+        word = "こんにちは"
+        lang = "ja"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "こんにちは")
+        
+    def test_thai_does_not_change_word(self):
+        word = "สวัสดี"
+        lang = "th"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "สวัสดี")
+        
+    def test_unsupported_language_lowercases_word(self):
+        word = "Macbook"
+        lang = "fr"
+        result = lowercase_word(word, lang)
+        self.assertEqual(result, "macbook")
+        
+if __name__ == '__main__':
+    unittest.main()
