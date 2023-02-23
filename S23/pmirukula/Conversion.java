30a9d00fa3c6eeff20b2b3d2bc485360ef8776ee
PMIRUKULA <112145548+PMIRUKULA@users.noreply.github.com>
2023-02-22 20:54:26 -0600

    Add files via upload

--- S23/pmirukula/Conversion.java
+++ S23/pmirukula/Conversion.java
@@ -5,8 +5,33 @@ import java.io.*;
 import java.util.Locale;
 
 public class Conversion {
-	public void conversionlogic() 
-	{
+	public static String trazConv(String str_lang, String str) {
+		if (str.contains("I")) {
+			str = str.replaceAll("I", "\u0131");
+			return str.toLowerCase(new Locale(str_lang));
+		} else {
+			return null;
+		}
+	}
+
+	public static String gaConv(String str_lang, String str) {
+
+		if (("nt".indexOf(str.charAt(0)) != -1)
+				&& ("AEIOUÁÉÚÍÓ".indexOf(str.charAt(1)) != -1))
+
+		{
+			String k;
+			k = str.toLowerCase(new Locale(str_lang));
+			String m = (k.substring(0, 1) + "-" + k.substring(1));
+			return m;
+		} else {
+			return null;
+		}
+
+	}
+
+	public static String allConv(String str_lang, String str) {
+		return (str.toLowerCase(new Locale(str_lang)));
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -15,27 +40,14 @@ public class Conversion {
 		String str = br.readLine();
 		System.out.println("Please enter the language you would like to translate the word");
 		String str_lang = br.readLine();
-		PrintWriter out = new PrintWriter("oceans.txt"); 
+		PrintWriter out = new PrintWriter("oceans.txt");
 		String output = str.toLowerCase(new Locale(str_lang));
-		if(str_lang == " tr " || str_lang == "az")
-		{
-			if(str.contains("I")) 
-			{
-				str = str.replaceAll("I", " \0u131");
-				output = str.toLowerCase(new Locale(str_lang));
-			}
-		}
-		else if(str_lang.equals("ga-IE")|| str_lang.equals("ga") )
-		{
-	
-			if((str.charAt(0) == 'n' || str.charAt(0) == 't')&& (str.charAt(1) == 'A' || str.charAt(1) == 'E' || str.charAt(1) == 'I' || str.charAt(1) == 'O' 
-			|| str.charAt(1) == 'U'|| str.charAt(1) == 'Ó' || str.charAt(1) == 'Á' ||str.charAt(1) == 'É' || str.charAt(1) == 'Ú' ||str.charAt(1) == 'Í'))
-			
-			{
-				
-				output = output.substring(0,1) + "-" + output.substring(1);
-			}
-			
+		if (str_lang.equals("tr") || str_lang.equals("az")) {
+			output = trazConv(str_lang, str);
+		} else if (str_lang.contains("ga")) {
+			output = gaConv(str_lang, str);
+		} else {
+			output = allConv(str_lang, str);
 		}
 		out.println(output);
 		out.close();
